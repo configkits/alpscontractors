c8c21c282545220c3c40944e27f4829b2bfb7186
riyajath-ahamed <riyajatha@gmail.com>
2026-02-10 22:23:35 +0530

    data | RJ

---
+++ src/data/index.ts
@@ -0,0 +1,36 @@
+export const data = {
+
+    quickLinks: [
+        {
+            label: "Industrial Resin",
+            href: "/services/industrial-resin"
+        },
+        {
+            label: "Waterproofing",
+            href: "/services/liquid-waterproofing"
+        },
+    ],
+
+    support: [
+        {
+            label: "Health & Safety",
+            href: "/services/health-and-safety"
+        },
+        {
+            label: "Technical Specs",
+            href: "/services/technical-specs"
+        },
+    ],
+
+    contact: [
+        {
+            label: "Phone",
+            href: "/services/phone"
+        },
+        {
+            label: "Email",
+            href: "/services/email"
+        },
+    ],
+
+}
