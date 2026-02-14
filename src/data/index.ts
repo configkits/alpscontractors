aa4d3539ff2e2d19d28eca1d9e54abba2f8f7844
riyajath-ahamed <riyajatha@gmail.com>
2026-02-15 02:29:04 +0530

    Data | RJ

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,81 +1,51 @@
-import Sika from "../assets/parters/g6-640w.png"
-import flowcreate from "../assets/parters/Group-2-1-640w.png"
-import mapei from "../assets/parters/Group-3-1-640w.png"
-import carboline from "../assets/parters/Group-1-1-640w.png"
+import Sika from "../assets/parters/g6-640w.png";
+import flowcreate from "../assets/parters/Group-2-1-640w.png";
+import mapei from "../assets/parters/Group-3-1-640w.png";
+import carboline from "../assets/parters/Group-1-1-640w.png";
 
 export const DATA = {
-
-    location:"Auckland, NZ",
-    linkedin: "https://www.linkedin.com/company/alpscontractors/",
-    facebook: "https://web.facebook.com/profile.php?id=100070726394822",
-    googlemap: "https://www.google.com/maps/place/Alps+Contractors/@-36.9026258,174.8196943,886m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4aeb10429427fe7:0xa19bcaabbec9c974!8m2!3d-36.9026258!4d174.8196943!16s%2Fg%2F11q8stv_9t!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D",
-
-    tel1: {
-        withCode: "+642108282760",
-        number: "0210 828 2760",
-    },
-
-    tel2: {
+  location: "Auckland, NZ",
+  linkedin: "https://www.linkedin.com/company/alpscontractors/",
+  facebook: "https://web.facebook.com/profile.php?id=100070726394822",
+  googlemap:
+    "https://www.google.com/maps/place/Alps+Contractors/@-36.9026258,174.8196943,886m/data=!3m2!1e3!4b1!4m6!3m5!1s0x4aeb10429427fe7:0xa19bcaabbec9c974!8m2!3d-36.9026258!4d174.8196943!16s%2Fg%2F11q8stv_9t!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D",
+  tel1: {
+    withCode: "+642108282760",
+    number: "0210 828 2760",
+    href: "tel:+642108282760"
+  },
+  tel2: {
     withCode: "+64212762761",
     number: "021 276 2761",
+    href: "tel:+64212762761"
+  },
+  email: "info@alpscontractors.co.nz",
+  partner: [
+    {
+      name: "Sika",
+      url: "https://nzl.sika.com/",
+      images: Sika,
     },
-
-    email: "info@alpscontractors.co.nz",
-    quickLinks: [
-        {
-            label: "Industrial Resin",
-            href: "/services/industrial-resin"
-        },
-        {
-            label: "Waterproofing",
-            href: "/services/liquid-waterproofing"
-        },
-    ],
-
-    support: [
-        {
-            label: "Health & Safety",
-            href: "/services/health-and-safety"
-        },
-        {
-            label: "Technical Specs",
-            href: "/services/technical-specs"
-        },
-    ],
-
-    contact: [
-        {
-            label: "Phone",
-            href: "/services/phone"
-        },
-        {
-            label: "Email",
-            href: "/services/email"
-        },
-    ],
-    partner:[
-        {
-            name:"Sika",
-            url:"https://nzl.sika.com/",
-            images:Sika
-        },
-        {
-            name:"Flowcreate",
-            url:"https://www.flowcrete.co.nz/",
-            images:flowcreate
-        },
-        {
-            name:"Mapei",
-            url:"https://www.mapei.com/nz/en/home-page",
-            images:mapei
-        },
-        {
-            name:"Carboline",
-            url:"https://www.carboline.com/",
-            images:carboline
-        }
-
-
-    ]
-
-}
+    {
+      name: "Flowcreate",
+      url: "https://www.flowcrete.co.nz/",
+      images: flowcreate,
+    },
+    {
+      name: "Mapei",
+      url: "https://www.mapei.com/nz/en/home-page",
+      images: mapei,
+    },
+    {
+      name: "Carboline",
+      url: "https://www.carboline.com/",
+      images: carboline,
+    },
+  ],
+  client: [
+    {
+      name: "lenards",
+      images: "",
+    },
+  ],
+};
