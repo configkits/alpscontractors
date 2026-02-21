b2a7318ad70431f09e4e1a1fb6508905d5595e95
riyajath-ahamed <riyajatha@gmail.com>
2026-02-21 17:21:23 +0530

    health, Slip | RJ

---
+++ src/pages/SlipResistanceTesting.tsx
@@ -0,0 +1,14 @@
+import Banner from '@/components/banner'
+import Navbar from '@/components/navbar'
+
+const SlipResistanceTesting = () => {
+  return (
+    <div>
+        <Banner />
+        <Navbar />
+        <h1 className="text-4xl font-bold text-center py-12">Slip Resistance Testing</h1>
+    </div>
+  )
+}
+
+export default SlipResistanceTesting
